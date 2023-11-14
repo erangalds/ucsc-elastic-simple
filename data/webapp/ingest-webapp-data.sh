4580e9e35c70188a94d86876b993245ba29f9e82
erangalds <erangalds@yahoo.com>
2023-11-14 15:19:24 +0530

    brought the modified ml data ingest script

--- data/webapp/ingest-webapp-data.sh
+++ data/webapp/ingest-webapp-data.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo /usr/share/logstash/bin/logstash -f /home/vagrant/webapp/webapp-logstash.conf < webapp.csv
+sudo /usr/share/logstash/bin/logstash -f /home/vagrant/webapp/webapp-tagged-logstash.conf < webapp-tagged.csv
