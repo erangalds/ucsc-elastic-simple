760a5ee6ccee80a99af9f932289144fc76fff729
erangalds <erangalds@yahoo.com>
2023-11-15 18:50:35 +0530

    added a script to change the service account token in elastic for kibana

--- scripts/manually-configure/change-elastic-user-password.sh
+++ scripts/manually-configure/change-elastic-user-password.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo /usr/share/elasticsearch/bin/elasticsearch-reset-password -i -u elastic --url https://elk-single-node:9200
+
+elastic_user_password=Ucsc@1234
+echo -e $elastic_user_password | sudo /usr/share/elasticsearch/bin/elasticsearch-reset-password -i -u elastic --url https://elk-single-node:9200
