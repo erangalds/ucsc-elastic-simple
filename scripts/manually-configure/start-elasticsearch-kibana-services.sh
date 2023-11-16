58263b250fbc11acf5fc1aeeaef3fa8f79673fe6
erangalds <erangalds@yahoo.com>
2023-11-16 19:19:54 +0530

    checking crlf conversion

--- scripts/manually-configure/start-elasticsearch-kibana-services.sh
+++ scripts/manually-configure/start-elasticsearch-kibana-services.sh
@@ -3,9 +3,9 @@ sudo systemctl daemon-reload
 sudo systemctl enable elasticsearch.service
 sudo systemctl start elasticsearch.service
 
-sleep 60
+sleep 75
 
 sudo systemctl enable kibana.service
 sudo systemctl start kibana.service
 
-sleep 60
+sleep 75
