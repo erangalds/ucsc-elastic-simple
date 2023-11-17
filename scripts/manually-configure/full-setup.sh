76a4412d4a6803db37f0078bea48a173040f95e0
erangalds <erangalds@yahoo.com>
2023-11-17 17:20:55 +0530

    before adding the logging enabled scripts

--- scripts/manually-configure/full-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-bash /home/vagrant/scripts/manually-configure/setup-elastic-service-account-token.sh
-bash /home/vagrant/scripts/manually-configure/start-elasticsearch-kibana-services.sh
-bash /home/vagrant/scripts/manually-configure/change-elastic-user-password.sh
